0f78215afa5faa9b0fcd6bd1fa196bdf4e4bcba1
abbiefinlayson1 <abbiefinlayson@monzo.com>
2025-03-11 16:46:07 +0000

    Add New Space Page

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
 from flask import redirect
+from forms.space_form import *
 
 # Create a new Flask app
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "This is a secret key"
 
 # Mock objects (until we have the DB built out)
 spaces_repository = Mock()
@@ -96,6 +98,24 @@ def index():
     # This just redirects to the spaces page
     return redirect("/spaces")
 
+@app.route("/spaces/new", methods=["GET", "POST"])
+def create_space():
+    form = SpaceForm()
+    if form.validate_on_submit():
+        new_space = {
+            "property_name": form.property_name.data,
+            "price_per_night": form.price_per_night.data,
+            "beds": form.beds.data,
+            "location": form.location.data,
+            "rating": form.rating.data,
+            "image_url": form.image_url.data,
+            "property_type": form.property_type.data,
+            "description": form.description.data,
+            "availability": form.availability.data,
+            "host_name": form.host_name.data}
+        spaces_repository.all.return_value.append(new_space)
+        return redirect("/spaces")
+    return render_template("spaces/new_space.html", form=form)
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
